6c3ee791217a67c3e01e63a3635eb066b8f98358
Blazzard <blazzard@gmail.com>
2019-10-02 16:15:03 -0600

    initial commit

---
+++ vstest1-project1/Pages/application.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace vstest1_project1.Pages
+{
+    public class applicationModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
